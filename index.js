39b23c5765b32ae1540d824489945f2d6b10bfd8
AgustinSalazar2 <agustinsssalazar@gmail.com>
2022-09-15 03:21:37 -0300

    Importacion de nuevos modulos 15/09/22

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//Importamos los modulos:
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+
+//Puertos:
 const port = 3000;
 
+
+//Middlewares:
+app.use(morgan('dev'));
+
+
+//Iniciar el servidor:
 app.listen(port, ()=> console.log(`Srvidor corriendo en http://localhost:${port}`)); 
